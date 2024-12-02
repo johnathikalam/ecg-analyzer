e1ef1ec0b904b64071944bd264eb1485f7361f2a
johnathikalam <johnathikalam3@gmail.com>
2024-12-02 11:05:36 +0530

    Start and stop button added

--- lib/Screens/data_receive_page.dart
+++ lib/Screens/data_receive_page.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -22,6 +23,7 @@ class _Message {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  static final clientID = 0;
   double rate = 0.0;
   BluetoothConnection? connection;
   bool isConnecting = true;
@@ -29,6 +31,7 @@ class _ChatPageState extends State<ChatPage> {
   bool isDisconnecting = false;
   List<_Message> messages = List<_Message>.empty(growable: true);
   String _messageBuffer = '';
+  bool startFlag = false;
 
   final TextEditingController textEditingController =
   TextEditingController();
@@ -90,6 +93,30 @@ class _ChatPageState extends State<ChatPage> {
       appBar: AppBar(title: Center(child: const Text('Receiving ECG data'))),
       body: Stack(
         children: [
+          startFlag ?
+          Positioned(
+            left: 20,
+            child:
+            IconButton.outlined(
+                icon:  Icon(Icons.pause, color: isConnected ? Colors.red : Colors.grey,),
+                onPressed: () {
+                  _sendMessage("0");
+                  startFlag = false;
+                  print(startFlag);
+                }
+                ),
+          ) :
+          Positioned(
+            left: 20,
+            child: IconButton.outlined(
+                color: Colors.black,
+                icon:  Icon(Icons.play_arrow,  color: isConnected ? Colors.green : Colors.grey,),
+                onPressed: isConnected ? () => {
+                  _sendMessage("1"),
+                  startFlag = true,
+                  print(startFlag),
+                } : null),
+          ),
           Center(child: Image.asset("assets/logo/heartbeat.gif", height: MediaQuery.of(context).size.height*.8)),
           Positioned(
             bottom: 60,
@@ -147,6 +174,33 @@ class _ChatPageState extends State<ChatPage> {
       _messageBuffer = (backspacesCounter > 0
           ? _messageBuffer.substring(0, _messageBuffer.length - backspacesCounter)
           : _messageBuffer + dataString);
+    }
+  }
+
+
+  void _sendMessage(String text) async {
+    text = text.trim();
+    textEditingController.clear();
+
+    if (text.length > 0) {
+      try {
+        connection!.output.add(Uint8List.fromList(utf8.encode(text + "\r\n")));
+        await connection!.output.allSent;
+
+        setState(() {
+          messages.add(_Message(clientID, text));
+        });
+
+        Future.delayed(Duration(milliseconds: 333)).then((_) {
+          listScrollController.animateTo(
+              listScrollController.position.maxScrollExtent,
+              duration: Duration(milliseconds: 333),
+              curve: Curves.easeOut);
+        });
+      } catch (e) {
+        // Ignore error, but notify state
+        setState(() {});
+      }
     }
   }
 
